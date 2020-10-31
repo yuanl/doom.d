62e52299a25161cbb5da1bcd9c23d4a32168a5ca
Yuanliang Li <yuanl.lee@gmail.com>
2020-10-31 21:28:50 +0800

    solarized theme looks good

--- config.el
+++ config.el
@@ -25,7 +25,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
+;; (setq doom-theme 'doom-dracula)
+(setq doom-theme 'doom-solarized-light)
 
 ;; Use a image as doom-dashboard.
 (when (featurep! :ui doom-dashboard)

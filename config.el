0fe5697ac8baa375773c6af9a947b0ab7a14a6f0
Yuanliang Li <yuanl.lee@gmail.com>
2021-03-09 11:04:35 +0800

    change my name

--- config.el
+++ config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Yuanl"
+(setq user-full-name "YuanLiang"
       user-mail-address "yuanl.lee@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
